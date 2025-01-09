858fcb4b80374db3d5592d44f8f380f22148243d
Gitte Beckmann <gitte.beckmann@hotmail.com>
2025-01-09 14:12:09 +0100

    - coded connection to MongoDB

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,13 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 //establish a connection to a MongoDB database with the mongoose.connect() method
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/legacy-lock";
-mongoose.connect(mongoUrl);
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost:27017/legacy-lock";
+mongoose.connect(mongoUrl)
+.then(() => console.log("MongoDB is connected"))
+.catch((err)=> console.log("error to connect MongoDB", err));
+
 mongoose.Promise = Promise;
 export default mongoose;
