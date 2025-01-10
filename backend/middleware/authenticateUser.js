78299849fa73736c453378b23c25748b0b9efa33
Gitte Beckmann <gitte.beckmann@hotmail.com>
2025-01-10 11:00:35 +0100

    - implemented authorization logic for logins

--- backend/middleware/authenticateUser.js
+++ backend/middleware/authenticateUser.js
@@ -2,12 +2,23 @@ import { User } from "../models/userSchema";
 
 // varifying users identity based on the users access token
 export const authenticateUser = async (req, res, next) => {
-  const user = await User.findOne({ accessToken: req.header("Authorization") });
-  if (user) {
+  //get the token from the authorization header
+  const token = req.header("Authorization");
+  // Check if a token exists
+  if(!token) {
+    return res.status(401),json({message: "No token provided"});
+  }
+  try{
+    //search for user with the access token
+    const user = await User.findOne({accessToken: token});
+ if (user) {
     req.user = user;
     next();
-  } else {
+    } else {
     res
-      .status(401).json({ loggedOut: true });
+      .status(401).json({ loggedOut: true, message: "Invalid or expired token" });
+    }
+  } catch (error) {
+    res.status(500),json({message: "Server error", error});
   }
 };
