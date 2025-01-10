54de49f7346fa3196adc08e4a1f55db164717571
Gitte Beckmann <gitte.beckmann@hotmail.com>
2025-01-10 10:36:32 +0100

    - added login function for user

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -37,3 +37,39 @@ res.status(201).json({
     res.status(500).json({ message: "Server error", error });
   }
 };
+
+//Login User
+export const loginUser = async (req, res) => {
+  try {
+    const { emailOrUsername, password} = req.body;
+
+    //check for email or username
+    const user = await User.findOne({
+      $or: [{email: emailOrUsername}, {username: emailOrUsername}]
+    });
+    if(!user) {
+      return res.status(400).json({message:"User not found"});
+    }
+
+    //compare the password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if(!isPasswordValid) {
+      return res.status(400).json({message: "Invalid password"});
+    }
+
+    const accessToken = user.accessToken;
+
+    res.status(200).json({
+      success: true,
+      message: "Login successful",
+      user: {
+        id: user_id,
+        email: user.email,
+        username: user.username,
+        accessToken: accessToken,
+      },
+    });
+  } catch (error) {
+      res.status(500),json({message: "Server error", error});
+  }
+};
