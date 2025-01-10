227d64a3e947066768331b5ca890d7e5c5e29488
Johanna Eriksson <johanna.eriksson@schibsted.com>
2025-01-10 14:00:14 +0100

    Add documentation comments on PublicRoutes file. Add all public routes to the PublicRoute file and delete them from App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,9 +11,6 @@ import CapsuleDetailsPage from './pages/CapsuleDetailsPage';
 const App = () => (
   <Router>
     <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignUpPage />} />
       <Route path="/dashboard" element={<DashboardPage />} />
       <Route path="/profile" element={<ProfileSettingsPage />} />
       <Route path="/capsules/create" element={<CreateCapsulePage />} />
