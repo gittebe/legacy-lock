c2020820637e2dc923c7bad441b7dbac9bb50dd4
Gitte Beckmann <gitte.beckmann@hotmail.com>
2025-01-10 11:19:31 +0100

    - corrected typos and added router for login and logout

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { registerUser } from "../controllers/authController.js";
+import { registerUser, loginUser, logoutUser } from "../controllers/authController.js";
 
 const router = express.Router();
 
 //POST request to register endpoint to create a new user
 router.post("/register", registerUser);
+
+//POST request for login
+router.post("/login", loginUser);
+
+//POST request for logout
+router.post("/logout", logoutUser)
 
 export default router;
