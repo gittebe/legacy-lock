2899552cdbfa0981eb046bb49c632e0636555946
Gitte Beckmann <gitte.beckmann@hotmail.com>
2025-01-11 07:19:12 +0100

    - changed to Atlas

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,8 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 //establish a connection to a MongoDB database with the mongoose.connect() method
-const mongoUrl = "mongodb://127.0.0.1:27017/legacy-lock";
-mongoose.connect(mongoUrl)
+// const mongoUrl = "mongodb://127.0.0.1:27017/legacy-lock";
+const mongoURI = process.env.MONGODB_URI
+
+mongoose.connect(mongoURI)
 
 .then(() => {
     console.log('MongoDB succesfully connected!');
