31733398d2e3a6f58daa70022fbbfe29cdd2fde5
Gitte Beckmann <gitte.beckmann@hotmail.com>
2025-01-14 20:16:54 +0100

    - added routes media.js and added multer configuration to be able to save the uploaded file

---
+++ backend/routes/media.js
@@ -0,0 +1,1 @@
+import {uploadMedia} from "../controllers/mediaController.js"
--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,16 @@ import "./config/db.js";
 
 import documentationRoutes from "./routes/documentation.js";
 import userRoutes from "./routes/users.js";
+import mediaRoutes from "./routes/media.js";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+//multer-configuration
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage});
 
 app.use(cors());
 app.use(express.json());
@@ -20,6 +25,10 @@ app.use(express.json());
 //use the imported routes
 app.use("/", documentationRoutes);
 app.use("/users", userRoutes);
+app.use("/media", mediaRoutes);
+
+//POST-endpoint to upload media
+app.post("/media/upload", upload.single("media"), uploadMedia)
 
 // Start the server
 app.listen(port, () => {
