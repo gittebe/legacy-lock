feb99d2e5ce80e1346bded56a82232f99b805497
Johanna Eriksson <johanna.eriksson@schibsted.com>
2025-01-18 20:13:05 +0100

    Add FormData to handle file, text, and unlockDate uploads according to Cloudinary documentation.

--- frontend/src/pages/CreateCapsulePage.jsx
+++ frontend/src/pages/CreateCapsulePage.jsx
@@ -2,10 +2,18 @@ import { useState } from "react";
 
 const CreateCapsulePage = () => {
   const fileInput = useRef(); // Create a reference to the Cloudinary file input  
-  const [text, setText] = useState("");
-  const [unlockDate, setUnlockDate] = useState("");
+  const [text, setText] = useState(""); // Create a state variable to store the text
+  const [unlockDate, setUnlockDate] = useState(""); // Create a state variable to store the unlock date
 
-  const handleMediaUpload = (event) => {
+  const handleMediaUpload = async (event) => { // Create a function to handle the media upload
+    event.preventDefault(); // Prevent the default form behavior
+
+    const formData = new FormData(); // Create a new FormData object
+    formData.append("file", fileInput.current.files[0]); // Append the file to the FormData object
+    formData.append("text", text ); // Append the upload the message to the FormData object
+    formData.append("unlockDate", unlockDate); // Append the unlock date to the FormData object
+
+    
 
   return (
     <div>
