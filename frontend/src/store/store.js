d02307fe3b13af48881b8aae5c7e1d139320d93b
Johanna Eriksson <johanna.eriksson@schibsted.com>
2025-01-19 20:02:53 +0100

    Update documentation comments on store.js to involve fetching capsules.

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -5,11 +5,17 @@
  * This store handles global authentication state.
  * 
  * Features:
- * - isLoggedIn: Tracks whether the user is logged in.
- * - user: Stores the logged-in user's object (e.g., email and password).
- * - set: A function provided by Zustand to update the store's state.
- * - login(user): Updates the store with user details and sets `isLoggedIn` to true.
- * - logout(): Resets the store to its initial state (logs out the user).
+ * 
+ * - Authentication:
+ *  - isLoggedIn: Tracks whether the user is logged in.
+ *  - user: Stores the logged-in user's object (e.g., email and password).
+ *  - set: A function provided by Zustand to update the store's state.
+ *  - login(user): Updates the store with user details and sets `isLoggedIn` to true.
+ *  - logout(): Resets the store to its initial state (logs out the user).
+ * - Capsules:
+ *  - capsules: Stores a list of the user's capsules.
+ *  - fetchCapsules(): Retrieves the user's capsules from the server.
+ *  - loading: Loading indicator to show whether the data is being fetched. 
  */
 
 import { create } from "zustand";
