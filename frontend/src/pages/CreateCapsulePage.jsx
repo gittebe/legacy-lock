f76a63a447e4c61b576cd21705bfcc4fb6bd7d58
Johanna Eriksson <johanna.eriksson@schibsted.com>
2025-01-20 11:11:01 +0100

    Change import of from button to Button.

--- frontend/src/pages/CreateCapsulePage.jsx
+++ frontend/src/pages/CreateCapsulePage.jsx
@@ -15,7 +15,7 @@
  **/
 
 import { useState, useRef } from "react";
-import Button from "../components/button";
+import Button from "../components/Button";
 
 const CreateCapsulePage = () => {
   // Create a reference to the Cloudinary file input:
