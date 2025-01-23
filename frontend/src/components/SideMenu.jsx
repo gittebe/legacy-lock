5b515278d772a70e7b590af24a77d835bd89f690
Gitte Beckmann <gitte.beckmann@hotmail.com>
2025-01-23 12:38:51 +0100

    - added Profile to sidemenu

--- frontend/src/components/SideMenu.jsx
+++ frontend/src/components/SideMenu.jsx
@@ -4,12 +4,17 @@ import { useNavigate } from "react-router-dom";
 
 export const SideMenu = ({ showMenu, toggleMenu, onLoginClick, onSignUpClick, onLogoutClick, isLoggedIn }) => {
   const navigate = useNavigate();
+  const handleProfileClick = () => {navigate("/profile")}
 
   return (
     <aside className={`side-menu ${showMenu ? 'open' : ""}`} onClick={toggleMenu}>
-      <div className="side-menu-content" onClick={(e) => e.stopPropagation()}>
+      <div className={`side-menu-content ${isLoggedIn ? 'logged-in' : ''}`}  onClick={(e) => e.stopPropagation()}>
         {isLoggedIn ? (
+          <>
+          <div className="menu-logged-in"></div>
+          <button onClick={handleProfileClick}>Profile</button>
           <button onClick = {onLogoutClick}>Sign out</button>
+          </>
         ) : (
           <>
           <button onClick={onLoginClick}>Log in</button>
