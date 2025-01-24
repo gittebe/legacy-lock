0be7862badc0c1c0b1418e9a4b4ffc4bd0576ac4
Gitte Beckmann <gitte.beckmann@hotmail.com>
2025-01-24 14:46:39 +0100

    - added icon button for header smaller than 667px (instead of hamburger menu)

--- frontend/src/ui/ProfileIcon.jsx
+++ frontend/src/ui/ProfileIcon.jsx
@@ -1,7 +1,9 @@
 import "./ProfileIcon.css";
 
 export const ProfileIcon = () => {
-    <div className="profile-icon-container">
+    return (
+         <div className="profile-icon-container">
         <img className="profile-icon" src = "../../profile-icon.png"/>
     </div>
+    )
 }
