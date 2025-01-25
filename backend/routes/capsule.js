69ffb81cec94e8915873569d45a0ced39933f7a1
Johanna Eriksson <johanna.eriksson@schibsted.com>
2025-01-26 00:06:27 +0100

    Save any unsaved changes

--- backend/routes/capsule.js
+++ backend/routes/capsule.js
@@ -2,17 +2,8 @@ import express from "express";
 import {createCapsule, getCapsule, getUserCapsules, getReceivedCapsules, getCreateCapsulePage} from "../controllers/capsuleController.js";
 import { authenticateUser } from "../middleware/authenticateUser.js";
 import upload from "../middleware/multer.js";
-import mongoose from "mongoose";
 
 const router = express.Router();
-
-// Middleware to validate ObjectID
-const validateObjectId = (req, res, next) => {
-    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-        return res.status(400).json({ message: "Backend: Invalid capsule ID format" });
-    }
-    next();
-};
 
 // Protected CreateCapsulePage Route (only for authenticated user)
 router.get("/", authenticateUser, getCreateCapsulePage);
