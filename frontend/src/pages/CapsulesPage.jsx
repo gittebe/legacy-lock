d334de1d022d6fb92c0b1fe76847f2ab3e52a7ea
Gitte Beckmann <gitte.beckmann@hotmail.com>
2025-01-26 16:52:29 +0100

    -

--- frontend/src/pages/CapsulesPage.jsx
+++ frontend/src/pages/CapsulesPage.jsx
@@ -3,11 +3,56 @@
  * Capsules page
  */
 
+import { useState, useEffect } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+import useStore from "../store/store";
+import { CapsuleList } from "../components/CapsuleList";
+import { Header } from "../components/Header";
+import { SideMenu } from "../components/SideMenu";
+import { CreateCapsule } from "../components/CreateCapsule";
+
 export const CapsulesPage = () => {
+  console.log("DashboardPage rendered");
+  const user = useStore((state) => state.user);
+  const logout = useStore((state) => state.logout);
+  const navigate = useNavigate();
+  const [showMenu, setShowMenu] = useState(false);
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+  const openPopup = () => setIsPopupOpen(true); 
+  const closePopup = () => setIsPopupOpen(false); 
+
+  // If user is not logedin
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+
+    const handleLogout = () => {
+      console.log("Logout button clicked");
+      logout(); // Call the logout method from your store
+      navigate("/"); // Redirect to home or login page
+    };
+
   return (
-    <div>
-      <h1>Capsules Page</h1>
-      <p>Welcome to the CapsulesPage!</p>
-    </div>
+    <>
+      <Header toggleMenu={() => setShowMenu(!showMenu)} />
+      <SideMenu
+        showMenu={showMenu}
+        toggleMenu={() => setShowMenu(false)}
+        onLogoutClick={handleLogout}
+        isLoggedIn={!!user} 
+      />
+      
+      <div>
+        <h1>Welcome, {user.username}!</h1>
+        <h2>Your Capsules</h2>
+        <CapsuleList />
+      </div>
+      <div>
+        <button onClick={openPopup}>Create Capsule</button>
+        <CreateCapsule isOpen={isPopupOpen} onClose={closePopup} />
+      </div>
+    </>
   );
 };
+
