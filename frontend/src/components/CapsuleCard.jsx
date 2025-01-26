7ba9d8335f1d279584a1c2b7f44541ddbbfc332e
Gitte Beckmann <gitte.beckmann@hotmail.com>
2025-01-26 16:20:52 +0100

    - added code for capsule card

---
+++ frontend/src/components/CapsuleCard.jsx
@@ -0,0 +1,28 @@
+import { useNavigate } from "react-router-dom";
+import { ViewCapsuleButton } from "../ui/ViewCapsuleButton";
+import { formatDateTime }  from "../utils/date";
+import "./CapsuleCard.css";
+import { CapsuleCardText } from "../ui/CapsuleCardText";
+import { CapsuleCardImage } from "../ui/CapsuleCardImage";
+
+export const CapsuleCard = ({ capsule }) => {
+  const { title, id, openAt, mediaUrls } = capsule;
+  const navigateToCapsule = useNavigate();
+
+  const handleViewCapsule = () => {
+    navigateToCapsule(`/capsules/${id}`);
+  };
+   const isCapsuleOpen = openAt ? new Date() >= new Date(openAt) : false
+
+  const formattedOpenAt = openAt
+    ? formatDateTime(new Date(openAt), "yyyy-MM-dd HH:mm")
+    : "Invalid date";
+
+  return (
+    <div className="capsule-card" onClick={handleViewCapsule}>
+      {/* Übergabe des Arrays an das Medien-Rendering */}
+      <CapsuleCardImage mediaUrls={mediaUrls} isBlurred={!isCapsuleOpen}/>
+      <CapsuleCardText title={title} openAt={formattedOpenAt} />
+    </div>
+  );
+};
