39b680020d8e8c25c220a6bc8453eea764676018
Gitte Beckmann <gitte.beckmann@hotmail.com>
2025-01-26 15:56:26 +0100

    - returned code

--- frontend/src/pages/CapsulesPage.jsx
+++ frontend/src/pages/CapsulesPage.jsx
@@ -2,3 +2,55 @@
  * 
  * Capsules page
  */
+
+import { useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+import useStore from "../store/store";
+import { CapsuleList } from "../components/CapsuleList";
+import { Header } from "../components/Header";
+import { SideMenu } from "../components/SideMenu";
+import { FooterMobile } from "../components/FooterMobile";
+
+export const CapsulesPage = () => {
+  console.log("CapsulesPage rendered");
+  const user = useStore((state) => state.user);
+  const logout = useStore((state) => state.logout);
+  const navigate = useNavigate();
+  const [showMenu, setShowMenu] = useState(false);
+
+  // If user is not logged in
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  const handleLogout = () => {
+    console.log("Logout button clicked");
+    logout(); // Call the logout method from your store
+    navigate("/"); // Redirect to home or login page
+  };
+
+  return (
+    <>
+      <Header toggleMenu={() => setShowMenu(!showMenu)} />
+      <SideMenu
+        showMenu={showMenu}
+        toggleMenu={() => setShowMenu(false)}
+        onLogoutClick={handleLogout}
+        isLoggedIn={!!user}
+      />
+
+      <div>
+        <h1>All Capsules</h1>
+
+        <h2>Created by You:</h2>
+        <CapsuleList filter="created" /> 
+
+        <h2>Received by You</h2>
+        <CapsuleList filter="received" />
+      </div>
+
+      <FooterMobile />
+    </>
+  );
+};
+
