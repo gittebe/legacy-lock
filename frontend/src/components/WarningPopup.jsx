c463ee6a7ae6847da66c70f7b60c44f1265039ba
Johanna Eriksson <johanna.eriksson@schibsted.com>
2025-01-29 20:07:09 +0100

    Import warningicon image to WarningPopup component. Add classNames for styling and text according to Figma file.

--- frontend/src/components/WarningPopup.jsx
+++ frontend/src/components/WarningPopup.jsx
@@ -1,11 +1,20 @@
 import "./WarningPopup.css";
+import warningIcon from "../assets/warningicon.png"; 
 
-export const WarningPopup = ({ message, onClose }) => {
+export const WarningPopup = ({ onClose }) => {
   return (
     <div className="popup-overlay">
       <div className="popup">
-        <p>{message}</p>
-        <button onClick={onClose}>OK</button>
+        <img src={warningIcon} alt="Warning icon" className="warning-icon" />
+        <br />
+        <br />
+        <p className="warning-title">Warning!</p>
+        <br />
+        <br />
+        <p className="warning-text">Can’t open a locket before its release date.</p>
+        <br />
+        <br />
+        <button onClick={onClose} className="popup-button">OK</button>
       </div>
     </div>
   );
