bba6ff3da61c06de6e04ebc249bf8a52f64ece90
jacquelinekellyhunt <kelly@cosafe.com>
2025-01-30 15:33:26 +0200

    Frontend: Add capsule details page

--- frontend/src/components/CapsuleDetailCard.jsx
+++ frontend/src/components/CapsuleDetailCard.jsx
@@ -1,58 +1,39 @@
-/**
- * CapsuleDetailsCard Component
- * Combines media and CapsuleCardImage functionality from CapsuleCard.
- */
-
-import { useNavigate } from "react-router-dom";
-import { formatDateTime } from "../utils/date";
-import { CapsuleCardImage } from "../ui/CapsuleCardImage";
+import "./CapsuleDetailCard.css";
 
 export const CapsuleDetailCard = ({ capsule }) => {
   const {
-    id,
     title,
     message,
     mediaUrls = [],
-    createdAt = null,
-    openAt = null,
     recipients = [],
   } = capsule.data || {};
 
-  console.log("Recipients data:", recipients);
-  console.log("Created At:", createdAt);
-  console.log("Open At:", openAt);
-
-  const navigateToCapsule = useNavigate();
-
-  const handleViewCapsule = () => {
-    navigateToCapsule(`/capsules/${id}`);
-  };
-
-  const isCapsuleOpen = openAt ? new Date() >= new Date(openAt) : false;
-  const formattedCreatedAt = createdAt
-    ? formatDateTime(new Date(createdAt), "yyyy-MM-dd HH:mm")
-    : "Invalid date";
-  const formattedOpenAt = openAt
-    ? formatDateTime(new Date(openAt), "yyyy-MM-dd HH:mm")
-    : "Invalid date";
-
   return (
-    <div className="capsule-detail-card" onClick={handleViewCapsule}>
-      <CapsuleCardImage mediaUrls={mediaUrls} isBlurred={!isCapsuleOpen} />
-      <h5>Title: {title}</h5>
-      <p>Message: {message}</p>
-      <p>Recipients:</p>
-      <ul>
-        {Array.isArray(recipients) && recipients.length > 0 ? (
-          recipients.map((recipient) => (
-            <li key={recipient._id}>{recipient.username}</li>
+    <div className="capsule-detail-card">
+      <h2 className="section-header">Photos</h2>
+      <div className="photos-container">
+        {mediaUrls.length > 0 ? (
+          mediaUrls.map((url, index) => (
+            <img
+              key={index}
+              src={url}
+              alt={`Capsule Photo ${index + 1}`}
+              className="photo"
+            />
           ))
         ) : (
-          <li>No recipients available</li>
+          <p>No photos available</p>
         )}
-      </ul>
-      <p>Created: {formattedCreatedAt}</p>
-      <p>Unlocks on: {formattedOpenAt}</p>
+      </div>
+
+      <h2 className="section-header">Messages</h2>
+      <div className="messages-container">
+        {message ? (
+          <div className="capsule-message">{message}</div>
+        ) : (
+          <p>No messages available</p>
+        )}
+      </div>
     </div>
   );
 };
