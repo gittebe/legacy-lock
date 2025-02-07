6f47dca961f3aaf66d70c0dc62dde79fea412b11
Johanna Eriksson <johanna.eriksson@schibsted.com>
2025-02-07 09:50:45 +0100

    Add first attempt to cut file name.

--- frontend/src/components/AttachmentIndicator.jsx
+++ frontend/src/components/AttachmentIndicator.jsx
@@ -1,10 +1,16 @@
 import "./AttachmentIndicator.css";
 
 export const AttachmentIndicator = ({ fileName }) => {
+  const maxLength = 15;
+  const shortenedFileName = 
+    fileName && fileName.length > maxLength 
+      ? fileName.slice(0, maxLength) + "..." 
+      : fileName || ""; 
+
   return (
     <div className="attachment-indicator">
       <span className="file-name">
-        <strong>Attachments:</strong> {fileName}
+        <strong>Attachments:</strong> {shortenedFileName}
       </span>
     </div>
   );
